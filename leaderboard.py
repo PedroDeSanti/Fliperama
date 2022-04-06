f3f8267dfc53be6641c9dad4fabcda8257a577f9
PedroDeSanti <phmartinsanti@gmail.com>
2022-04-05 22:22:44 -0300

    🔥 Remove unnecessary comments

--- leaderboard.py
+++ leaderboard.py
@@ -1,36 +1,5 @@
-# import os
-
 from pymongo import MongoClient, DESCENDING
-
-
 class LeaderBoard:
-
-    # client = MongoClient("mongodb+srv://teste:teste@cluster0.vpoht.mongodb.net/fliperama_db?retryWrites=true&w=majority")
-    # db = client.get_database('fliperama_db')
-    # scores =  db.score
-
-
-    #ou
-    # db = client['fliperama_db']
-    # collection = db['score']
-
-
-    # uri = "mongodb://test1:test1@ds051990.mongolab.com:51990/joran1"
-    # my_db_cli = MongoClient(uri)
-    # db = my_db_cli.joran1  # select the database ... 
-
-    # my_scores = db.scores  # this will be created if it doesn't exist!
-    # # add a new score
-    # my_scores.insert({"user_name": "Leeeeroy Jenkins", "score": 124, "time": "11/24/2014 13:43:22"})
-    # my_scores.insert({"user_name": "bob smith", "score": 88, "time": "11/24/2014 13:43:22"})
-
-    # # get a list of high scores (from best to worst)
-    # print(list(scores.find().sort("score", DESCENDING)))
-
-
-    # def create_leaderboard():
-    #     return True
-
 
     def __init__(self):
         self.client = MongoClient("mongodb+srv://teste:teste@cluster0.vpoht.mongodb.net/fliperama_db?retryWrites=true&w=majority")
@@ -38,52 +7,27 @@ class LeaderBoard:
         self.scores = self.db.score
 
     def get(self, difficult):
-        # here = os.path.dirname(os.path.abspath(__file__))
-        # filename = os.path.join(here, "map.txt")
-        # file = open(filename)
-
-        # file = open("teste.lb",'w')
-        # file.close()
-
-        # names = []
-        # score = []
-
-        # with open('teste.txt', 'r') as file:
-        #     content = file.readlines()
-        #     for line in content:
-        #         for char in line:
         player = []
         score_list = []
 
-        # lista = list(self.scores.find({"difficult": difficult}).sort("score", DESCENDING))
-        lista = list(self.scores.find().sort("score", DESCENDING))
-        # print(lista[0])
+        lista = list(self.scores.find({"difficult": difficult}).sort("score", DESCENDING))
+        # For testing
+        # lista = list(self.scores.find().sort("score", DESCENDING))
         for item in lista:
-            # print(item)
             for field in item:
                 if (field == 'nick'):
                     player.append(item[field])
                 if (field == 'score'):
                     player.append(item[field])
-                # print("player: ", player)
             score_list.append(player)
-            # print("score_list: ", score_list)
             player = []
-            # print("score_list: ", score_list)
-        # print(lista)
 
-        # print("score_list: ", score_list)
         return score_list
-        #
-
-        # return True
-
 
     def add(self, nick, score, difficult):
         doc_count = self.scores.count_documents({})
         id_number = doc_count + 1
         self.scores.insert_one({"_id": id_number, "nick": nick, "score": score, "difficult": difficult})
-
 
 # For testing:
 
@@ -98,6 +42,5 @@ class LeaderBoard:
 # leader_board.add("Lin", 1700, 1)
 # leader_board.add("Aux", 12000, 1)
 # leader_board.get(2)
-
 
 #-m pip install "pymongo[srv]"
